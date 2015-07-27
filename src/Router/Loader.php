ab188eca19fc40ecad11d8f5bc5ab807b1df96a9
VeeeneX <veeenex@gmail.com>
2015-07-27 20:19:08 +0200

    Identation fix

--- src/Router/Loader.php
+++ src/Router/Loader.php
@@ -6,44 +6,44 @@ use Aurora\Router;
 
 class Loader
 {
-   public $data;
+    public $data;
 
-   public function __construct(array $data)
-   {
-      $this->data = $data;
-   }
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
 
-   public function load()
-   {
-      $Router = new Router();
-      $type = (
-         isset($this->data["type"])
-         && ($this->data["type"] === "uri" || $this->data["type"] === "method")
-      ) ? $this->data["type"] : "uri";
+    public function load()
+    {
+        $Router = new Router();
+        $type = (
+        isset($this->data["type"])
+        && ($this->data["type"] === "uri" || $this->data["type"] === "method")
+        ) ? $this->data["type"] : "uri";
 
-      if (isset($this->data["matchTypes"])) {
-         $Router->addMatchTypes($this->data["matchTypes"]);
-      }
+        if (isset($this->data["matchTypes"])) {
+            $Router->addMatchTypes($this->data["matchTypes"]);
+        }
 
-      if (isset($this->data["baseUri"])) {
-         $Router->setBaseUri($this->data["baseUri"]);
-      }
+        if (isset($this->data["baseUri"])) {
+            $Router->setBaseUri($this->data["baseUri"]);
+        }
 
-      $routes = $this->data["routes"];
+        $routes = $this->data["routes"];
 
-      if ($type) {
-         foreach ($routes as $pattern => $data) {
-            $Router->addRoute($data[0], $pattern, $data[1]);
-         }
-      } else {
-         foreach ($routes as $method => $routesInMethod) {
-            foreach ($routesInMethod as $route) {
-               $Router->addRoute($method, $route[0], $route[1]);
+        if ($type) {
+            foreach ($routes as $pattern => $data) {
+                $Router->addRoute($data[0], $pattern, $data[1]);
             }
-         }
-      }
+        } else {
+            foreach ($routes as $method => $routesInMethod) {
+                foreach ($routesInMethod as $route) {
+                    $Router->addRoute($method, $route[0], $route[1]);
+                }
+            }
+        }
 
-      return $Router;
-   }
+        return $Router;
+    }
 
 }
